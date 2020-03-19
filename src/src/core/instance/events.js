9f9a07d48a87c2cb83f030dd285fcb4fe3752d25
chen Lin <linchen@yixin.im>
2020-03-19 17:42:39 +0800

    initEvent

--- src/src/core/instance/events.js
+++ src/src/core/instance/events.js
@@ -10,11 +10,18 @@ import {
 import { updateListeners } from '../vdom/helpers/index'
 
 export function initEvents(vm: Component) {
+    // 存储的是自定义事件
     vm._events = Object.create(null)
     vm._hasHookEvent = false
     // init parent attached events
+    // _parentListeners在/core/instance/init.js的initInternalComponent方法给赋了值
+    // 其来源于父VNode，而VNode来自于/core/vdom/create-component.js createComponent方法new VNode时传入的data.on
+    // data.on存储的是自定义事件，data.nativeOn存储的是原生浏览器事件
+    // 也就是_parentListeners存储的是父组件绑定的非原生浏览器事件
     const listeners = vm.$options._parentListeners
     if (listeners) {
+        // 存在的话就更新组件事件
+        // 其实这里是发布订阅模式
         updateComponentListeners(vm, listeners)
     }
 }
@@ -38,23 +45,31 @@ export function updateComponentListeners(
     listeners: Object,
     oldListeners: ?Object
 ) {
+    // 存储当前事件发生的组件实例对象
     target = vm
+    // 更新事件
     updateListeners(listeners, oldListeners || {}, add, remove, vm)
     target = undefined
 }
 // 混入事件方法
+// 其实这里是发布订阅模式，_events就是事件中心，事件都存储在此
 export function eventsMixin(Vue: Class<Component>) {
+    // 用于检测是否是hook事件
     const hookRE = /^hook:/
     Vue.prototype.$on = function (event: string | Array<string>, fn: Function): Component {
         const vm: Component = this
+        // this.$on(['test1', 'test2'], function() {  })
         if (Array.isArray(event)) {
             for (let i = 0, l = event.length; i < l; i++) {
                 this.$on(event[i], fn)
             }
         } else {
+            // 事件入队
             (vm._events[event] || (vm._events[event] = [])).push(fn)
             // optimize hook:event cost by using a boolean flag marked at registration
             // instead of a hash lookup
+            // 这里做个优化其实就是设置一个实例属性_hasHookEvent，只要判断到这个实例对象有注册了hook事件就置为true
+            // 这样子就可以在callHook(vm, 'beforeCreate')之类时不用callHook(vm, 'hook:beforeCreate')，只要在callHook判断下_hasHookEvent然后加个前缀即可
             if (hookRE.test(event)) {
                 vm._hasHookEvent = true
             }
@@ -64,7 +79,9 @@ export function eventsMixin(Vue: Class<Component>) {
 
     Vue.prototype.$once = function (event: string, fn: Function): Component {
         const vm: Component = this
+        // 劫持下下回调
         function on() {
+            // 这就是$once的秘密，它在回调触发之后就会$off
             vm.$off(event, on)
             fn.apply(vm, arguments)
         }
@@ -76,11 +93,13 @@ export function eventsMixin(Vue: Class<Component>) {
     Vue.prototype.$off = function (event?: string | Array<string>, fn?: Function): Component {
         const vm: Component = this
         // all
+        // this.$off()
         if (!arguments.length) {
             vm._events = Object.create(null)
             return vm
         }
         // array of events
+        // this.$off(['test1', 'test2'], fn)
         if (Array.isArray(event)) {
             for (let i = 0, l = event.length; i < l; i++) {
                 this.$off(event[i], fn)
@@ -89,9 +108,11 @@ export function eventsMixin(Vue: Class<Component>) {
         }
         // specific event
         const cbs = vm._events[event]
+        // 时间不存在
         if (!cbs) {
             return vm
         }
+        // 要取消的回调不存在
         if (!fn) {
             vm._events[event] = null
             return vm
@@ -102,6 +123,8 @@ export function eventsMixin(Vue: Class<Component>) {
             let i = cbs.length
             while (i--) {
                 cb = cbs[i]
+                // cb.fn其实是给$once使用的
+                // 这里就是判断下要退订的fn和已注册的是否一致
                 if (cb === fn || cb.fn === fn) {
                     cbs.splice(i, 1)
                     break
@@ -115,6 +138,9 @@ export function eventsMixin(Vue: Class<Component>) {
         const vm: Component = this
         if (process.env.NODE_ENV !== 'production') {
             const lowerCaseEvent = event.toLowerCase()
+            // https://cn.vuejs.org/v2/guide/components-custom-events.html#%E4%BA%8B%E4%BB%B6%E5%90%8D
+            // 这里其实是事件名kebab-case vs camelCase 或 PascalCase
+            // 也就是HTML不区分大小写，所以建议使用kebab-case而不是驼峰命名法
             if (lowerCaseEvent !== event && vm._events[lowerCaseEvent]) {
                 tip(
                     `Event "${lowerCaseEvent}" is emitted in component ` +
@@ -125,9 +151,11 @@ export function eventsMixin(Vue: Class<Component>) {
                 )
             }
         }
+        // 取到事件回调
         let cbs = vm._events[event]
         if (cbs) {
             cbs = cbs.length > 1 ? toArray(cbs) : cbs
+            // 取参数
             const args = toArray(arguments, 1)
             for (let i = 0, l = cbs.length; i < l; i++) {
                 try {
--- src/src/core/vdom/helpers/update-listeners.js
+++ src/src/core/vdom/helpers/update-listeners.js
@@ -25,7 +25,8 @@ const normalizeEvent = cached((name: string): {
         passive
     }
 })
-
+// 高阶函数，就是接受一个事件回调然后返回一个函数
+// 因为该事件回调可能是数组，若确定是函数的话就不要这层包装了
 export function createFnInvoker(fns: Function | Array<Function>): Function {
     function invoker() {
         const fns = invoker.fns
@@ -42,7 +43,7 @@ export function createFnInvoker(fns: Function | Array<Function>): Function {
     invoker.fns = fns
     return invoker
 }
-
+// 其实就是判断listeners和oldListeners然后对事件进行相应的注册和卸载
 export function updateListeners(
     on: Object,
     oldOn: Object,
@@ -50,32 +51,49 @@ export function updateListeners(
     remove: Function,
     vm: Component
 ) {
+    // def为cur副本，给weex框架处理参数使用
     let name, def, cur, old, event
+    // 以listeners为视角遍历
     for (name in on) {
+        // 存储新的事件对象，def为副本
         def = cur = on[name]
         old = oldOn[name]
+        // 解析event修饰符情况
         event = normalizeEvent(name)
         /* istanbul ignore if */
+        // weex框架处理，其添加事件可传params
+        // 值得注意的是非weex是没有params的
         if (__WEEX__ && isPlainObject(def)) {
             cur = def.handler
             event.params = def.params
         }
         if (isUndef(cur)) {
+            // 若是新的事件不存在就报警告
             process.env.NODE_ENV !== 'production' && warn(
                 `Invalid handler for event "${event.name}": got ` + String(cur),
                 vm
             )
         } else if (isUndef(old)) {
+            // 若是新的存在就得不存在那么就得添加新的事件了
             if (isUndef(cur.fns)) {
+                // 包装一下事件回调
+                // 这里关键在与on[name]赋值，是新事件的FnInvoker
                 cur = on[name] = createFnInvoker(cur)
             }
+            // 调用传入的add方法注册时间
+            // 不同的框架传入的add自然也是不一样的
             add(event.name, cur, event.once, event.capture, event.passive, event.params)
         } else if (cur !== old) {
+            // 若是新旧都存在，那么就改下事件的回调就行了
+            // 就像点击事件，新的只是改了点击事件的回调而已，不需要重新绑定点击事件
             old.fns = cur
+            // 这时候old是新的了，所以赋值给on，这样子on也是新的了
             on[name] = old
         }
     }
+    // 遍历旧的事件列表
     for (name in oldOn) {
+        // 若是这个事件在新的里面不存在说明是多余的需要去除
         if (isUndef(on[name])) {
             event = normalizeEvent(name)
             remove(event.name, oldOn[name], event.capture)

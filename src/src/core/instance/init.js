540261b62244af0a57c14bc461399c88d625a09c
chen Lin <linchen@yixin.im>
2020-03-19 09:52:29 +0800

    format

--- src/src/core/instance/init.js
+++ src/src/core/instance/init.js
@@ -12,220 +12,220 @@ import { extend, mergeOptions, formatComponentName } from '../util/index'
 
 let uid = 0
 // 混入init方法
-export function initMixin (Vue: Class<Component>) {
-  Vue.prototype._init = function (options?: Object) {
-    const vm: Component = this
-    // a uid
-    // 添加实例属性_uid，这是实例的唯一标志
-    vm._uid = uid++
+export function initMixin(Vue: Class<Component>) {
+    Vue.prototype._init = function (options?: Object) {
+        const vm: Component = this
+        // a uid
+        // 添加实例属性_uid，这是实例的唯一标志
+        vm._uid = uid++
 
-    let startTag, endTag
-    /* istanbul ignore if */
-    if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-      startTag = `vue-perf-start:${vm._uid}`
-      endTag = `vue-perf-end:${vm._uid}`
-      mark(startTag)
-    }
+        let startTag, endTag
+        /* istanbul ignore if */
+        if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
+            startTag = `vue-perf-start:${vm._uid}`
+            endTag = `vue-perf-end:${vm._uid}`
+            mark(startTag)
+        }
 
-    // a flag to avoid this being observed
-    // 判断对象是否是Vue实例对象，可用于Vue观测数据时避免观测Vue实例对象
-    vm._isVue = true
-    // merge options
-    // 判断是否是组件对象的，创建组件的时候会传入包含_isComponent属性，值为true的option
-    if (options && options._isComponent) {
-      // optimize internal component instantiation
-      // since dynamic options merging is pretty slow, and none of the
-      // internal component options needs special treatment.
-      initInternalComponent(vm, options)
-    } else {
-      vm.$options = mergeOptions(
-        resolveConstructorOptions(vm.constructor),
-        options || {},
-        vm
-      )
-    }
-    /* istanbul ignore else */
-    if (process.env.NODE_ENV !== 'production') {
-      initProxy(vm)
-    } else {
-      vm._renderProxy = vm
-    }
-    // expose real self
-    vm._self = vm
-    initLifecycle(vm)
-    initEvents(vm)
-    initRender(vm)
-    callHook(vm, 'beforeCreate')
-    initInjections(vm) // resolve injections before data/props
-    initState(vm)
-    initProvide(vm) // resolve provide after data/props
-    callHook(vm, 'created')
+        // a flag to avoid this being observed
+        // 判断对象是否是Vue实例对象，可用于Vue观测数据时避免观测Vue实例对象
+        vm._isVue = true
+        // merge options
+        // 判断是否是组件对象的，创建组件的时候会传入包含_isComponent属性，值为true的option
+        if (options && options._isComponent) {
+            // optimize internal component instantiation
+            // since dynamic options merging is pretty slow, and none of the
+            // internal component options needs special treatment.
+            initInternalComponent(vm, options)
+        } else {
+            vm.$options = mergeOptions(
+                resolveConstructorOptions(vm.constructor),
+                options || {},
+                vm
+            )
+        }
+        /* istanbul ignore else */
+        if (process.env.NODE_ENV !== 'production') {
+            initProxy(vm)
+        } else {
+            vm._renderProxy = vm
+        }
+        // expose real self
+        vm._self = vm
+        initLifecycle(vm)
+        initEvents(vm)
+        initRender(vm)
+        callHook(vm, 'beforeCreate')
+        initInjections(vm) // resolve injections before data/props
+        initState(vm)
+        initProvide(vm) // resolve provide after data/props
+        callHook(vm, 'created')
 
-    /* istanbul ignore if */
-    if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-      vm._name = formatComponentName(vm, false)
-      mark(endTag)
-      measure(`vue ${vm._name} init`, startTag, endTag)
-    }
+        /* istanbul ignore if */
+        if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
+            vm._name = formatComponentName(vm, false)
+            mark(endTag)
+            measure(`vue ${vm._name} init`, startTag, endTag)
+        }
 
-    if (vm.$options.el) {
-      vm.$mount(vm.$options.el)
+        if (vm.$options.el) {
+            vm.$mount(vm.$options.el)
+        }
     }
-  }
 }
 
-export function initInternalComponent (vm: Component, options: InternalComponentOptions) {
-  const opts = vm.$options = Object.create(vm.constructor.options)
-  // doing this because it's faster than dynamic enumeration.
-  const parentVnode = options._parentVnode
-  opts.parent = options.parent
-  opts._parentVnode = parentVnode
+export function initInternalComponent(vm: Component, options: InternalComponentOptions) {
+    const opts = vm.$options = Object.create(vm.constructor.options)
+    // doing this because it's faster than dynamic enumeration.
+    const parentVnode = options._parentVnode
+    opts.parent = options.parent
+    opts._parentVnode = parentVnode
 
-  const vnodeComponentOptions = parentVnode.componentOptions
-  opts.propsData = vnodeComponentOptions.propsData
-  opts._parentListeners = vnodeComponentOptions.listeners
-  opts._renderChildren = vnodeComponentOptions.children
-  opts._componentTag = vnodeComponentOptions.tag
+    const vnodeComponentOptions = parentVnode.componentOptions
+    opts.propsData = vnodeComponentOptions.propsData
+    opts._parentListeners = vnodeComponentOptions.listeners
+    opts._renderChildren = vnodeComponentOptions.children
+    opts._componentTag = vnodeComponentOptions.tag
 
-  if (options.render) {
-    opts.render = options.render
-    opts.staticRenderFns = options.staticRenderFns
-  }
+    if (options.render) {
+        opts.render = options.render
+        opts.staticRenderFns = options.staticRenderFns
+    }
 }
 // 获取Vue或者其子类的options
-export function resolveConstructorOptions (Ctor: Class<Component>) {
-  let options = Ctor.options
-  if (Ctor.super) {
-    //ø 若是子类的话，这里很绕，来个例子
-    /**
-    const fn1 = function fn1() {
-        console.log('fn1')
+export function resolveConstructorOptions(Ctor: Class<Component>) {
+    let options = Ctor.options
+    if (Ctor.super) {
+        //ø 若是子类的话，这里很绕，来个例子
+        /**
+        const fn1 = function fn1() {
+            console.log('fn1')
+        }
+        const fn2 = function fn2() {
+            console.log('fn2')
+        }
+        const fn3 = function fn3() {
+            console.log('fn3')
+        }
+        const fn4 = function fn4() {
+            console.log('fn4')
+        }
+        const Parent = Vue.extend({
+            template: '<p>1</p>',
+            created: [fn1]
+        })
+        const Child = Parent.extend({
+            template: '<p>2</p>',
+            created: [fn2]
+        })
+        Vue.mixin({
+            template: '<p>3</p>',
+            created: [fn3]
+        })
+        Child.mixin({
+            template: '<p>4</p>',
+            created: [fn4]
+        })
+        new Child({}).$mount('#app')
+         */
+        //ø 不算下面modifiedOptions这里的调用的话，这里的Ctor就是Child，Ctor.super就是Parent
+        // 也就是当前Parent.options应该的值
+        /*
+         superOptions当然就是合并之后的
+         {
+            components: {},
+            directives: {},
+            filters: {},
+            _base: function () {},
+            template: "<p>1</p>",
+            created: [fn3, fn1]
+        }
+        */
+        const superOptions = resolveConstructorOptions(Ctor.super)
+        // cachedSuperOptions在extend.js我们可以找到Sub.superOptions = Super.options，也就是cachedSuperOptions就是Parent.options当时的值
+        // 为什么是当时呢，这是因为Vue.extend之后Parent.mixin可以把Parent.options的引用指向别的地址
+        // 当时还未执行到Vue.mixin，所以除了Vue上默认的options也只是混入了.extend传入的参数
+        /*
+         恩，也就是cachedSuperOptions就是
+         {
+              components: {},
+              directives: {},
+              filters: {},
+              _base: function () {},
+              template: "<p>1</p>",
+              created: [fn1]
+          }
+         */
+        const cachedSuperOptions = Ctor.superOptions
+        //ø 俩个必然不一样的，其实不管内容如何，就是Vue.mixin({})，这俩值也不一样，因为Vue.options的引用变了（看Vue.mixin源码即可知）
+        if (superOptions !== cachedSuperOptions) {
+            // super option changed,
+            // need to resolve new options.
+            // 既然不一样了自然是要修正
+            Ctor.superOptions = superOptions
+            // check if there are any late-modified/attached options (#4976)
+            // 获取非父类继承来的options
+            const modifiedOptions = resolveModifiedOptions(Ctor)
+            // update base extend options
+            // 把新增的合并到Ctor.extendOptions，即Child.extendOptions
+            if (modifiedOptions) {
+                extend(Ctor.extendOptions, modifiedOptions)
+            }
+            // 以上可知superOptions是Parent.options的值，这个相对Child而言就是父类需要遗传自己的值
+            // Ctor.extendOptions就是自己需要合并的值
+            // 根据这俩值得到新选项赋值给options和Ctor.options
+            options = Ctor.options = mergeOptions(superOptions, Ctor.extendOptions)
+            if (options.name) {
+                // 这里源于.extend里会判断是否传入了name，若有的话会给自己添加到components
+                // 这里是修正下引用，懒得找哪里导致这个引用有问题了
+                options.components[options.name] = Ctor
+            }
+        }
     }
-    const fn2 = function fn2() {
-        console.log('fn2')
-    }
-    const fn3 = function fn3() {
-        console.log('fn3')
-    }
-    const fn4 = function fn4() {
-        console.log('fn4')
-    }
-    const Parent = Vue.extend({
-        template: '<p>1</p>',
-        created: [fn1]
-    })
-    const Child = Parent.extend({
-        template: '<p>2</p>',
-        created: [fn2]
-    })
-    Vue.mixin({
-        template: '<p>3</p>',
-        created: [fn3]
-    })
-    Child.mixin({
-        template: '<p>4</p>',
-        created: [fn4]
-    })
-    new Child({}).$mount('#app')
-     */
-    //ø 不算下面modifiedOptions这里的调用的话，这里的Ctor就是Child，Ctor.super就是Parent
-    // 也就是当前Parent.options应该的值
-    /*
-     superOptions当然就是合并之后的
-     {
-        components: {},
-        directives: {},
-        filters: {},
-        _base: function () {},
-        template: "<p>1</p>",
-        created: [fn3, fn1]
-    }
-    */
-   const superOptions = resolveConstructorOptions(Ctor.super)
-  // cachedSuperOptions在extend.js我们可以找到Sub.superOptions = Super.options，也就是cachedSuperOptions就是Parent.options当时的值
-  // 为什么是当时呢，这是因为Vue.extend之后Parent.mixin可以把Parent.options的引用指向别的地址
-  // 当时还未执行到Vue.mixin，所以除了Vue上默认的options也只是混入了.extend传入的参数
-   /*
-    恩，也就是cachedSuperOptions就是
-    {
-         components: {},
-         directives: {},
-         filters: {},
-         _base: function () {},
-         template: "<p>1</p>",
-         created: [fn1]
-     }
-    */
-    const cachedSuperOptions = Ctor.superOptions
-    //ø 俩个必然不一样的，其实不管内容如何，就是Vue.mixin({})，这俩值也不一样，因为Vue.options的引用变了（看Vue.mixin源码即可知）
-    if (superOptions !== cachedSuperOptions) {
-      // super option changed,
-      // need to resolve new options.
-      // 既然不一样了自然是要修正
-      Ctor.superOptions = superOptions
-      // check if there are any late-modified/attached options (#4976)
-      // 获取非父类继承来的options
-      const modifiedOptions = resolveModifiedOptions(Ctor)
-      // update base extend options
-      // 把新增的合并到Ctor.extendOptions，即Child.extendOptions
-      if (modifiedOptions) {
-        extend(Ctor.extendOptions, modifiedOptions)
-      }
-      // 以上可知superOptions是Parent.options的值，这个相对Child而言就是父类需要遗传自己的值
-      // Ctor.extendOptions就是自己需要合并的值
-      // 根据这俩值得到新选项赋值给options和Ctor.options
-      options = Ctor.options = mergeOptions(superOptions, Ctor.extendOptions)
-      if (options.name) {
-        // 这里源于.extend里会判断是否传入了name，若有的话会给自己添加到components
-        // 这里是修正下引用，懒得找哪里导致这个引用有问题了
-        options.components[options.name] = Ctor
-      }
-    }
-  }
-  return options
+    return options
 }
 // 处理修改过的
-function resolveModifiedOptions (Ctor: Class<Component>): ?Object {
-  let modified
-  // 承接resolveConstructorOptions里的栗子
-  /**
-   如今的（Child.mixin之后的）Child.options
-   没有fn3是因为Vue.mixin执行靠后了
-  {
-    components: {},
-    directives: {},
-    filters: {},
-    _base: function () {},
-    template: "<p4</p>",
-    created: [fn1, fn2, fn4]
-  }
-  */
-  const latest = Ctor.options
-  // 创建Ctor（Child）时调用.extend传的参数   { template: '<p>2</p>', created: [fn2] }
-  const extended = Ctor.extendOptions
-  /**
-  创建Ctor（Child）时调用.extend之后合并的options
-  {
-    components: {},
-    directives: {},
-    filters: {},
-    _base: function () {},
-    template: "<p>2</p>",
-    created: [fn1, fn2]
-  }
-  */
-  const sealed = Ctor.sealedOptions
-  // 遍历现在的options
-  for (const key in latest) {
-    if (latest[key] !== sealed[key]) {
-      // 要是Vue.extend里传的和现在的不一样那么就得修正
-      if (!modified) modified = {}
-      // 其实这里处理的是生命周期钩子重复问题
-      // 首先latest已经是最新值，值是没啥问题，值是若是生命周期钩子的话可能有重复
-      modified[key] = dedupe(latest[key], extended[key], sealed[key])
+function resolveModifiedOptions(Ctor: Class<Component>): ?Object {
+    let modified
+    // 承接resolveConstructorOptions里的栗子
+    /**
+     如今的（Child.mixin之后的）Child.options
+     没有fn3是因为Vue.mixin执行靠后了
+    {
+      components: {},
+      directives: {},
+      filters: {},
+      _base: function () {},
+      template: "<p4</p>",
+      created: [fn1, fn2, fn4]
     }
-  }
-  return modified
+    */
+    const latest = Ctor.options
+    // 创建Ctor（Child）时调用.extend传的参数   { template: '<p>2</p>', created: [fn2] }
+    const extended = Ctor.extendOptions
+    /**
+    创建Ctor（Child）时调用.extend之后合并的options
+    {
+      components: {},
+      directives: {},
+      filters: {},
+      _base: function () {},
+      template: "<p>2</p>",
+      created: [fn1, fn2]
+    }
+    */
+    const sealed = Ctor.sealedOptions
+    // 遍历现在的options
+    for (const key in latest) {
+        if (latest[key] !== sealed[key]) {
+            // 要是Vue.extend里传的和现在的不一样那么就得修正
+            if (!modified) modified = {}
+            // 其实这里处理的是生命周期钩子重复问题
+            // 首先latest已经是最新值，值是没啥问题，值是若是生命周期钩子的话可能有重复
+            modified[key] = dedupe(latest[key], extended[key], sealed[key])
+        }
+    }
+    return modified
 }
 
 /**
@@ -236,31 +236,31 @@ function resolveModifiedOptions (Ctor: Class<Component>): ?Object {
  * @param {*} sealed extend之后的值
  * @returns
  */
-function dedupe (latest, extended, sealed) {
-  // compare latest and sealed to ensure lifecycle hooks won't be duplicated
-  // between merges
-  if (Array.isArray(latest)) {
-    // 因为是数组，那么一般都是生命周期钩子函数，去重就是保证不要重复触发
-    const res = []
-    // 承接上个栗子
-    // [fn1, fn2]
-    sealed = Array.isArray(sealed) ? sealed : [sealed]
-    // [fn2]
-    extended = Array.isArray(extended) ? extended : [extended]
-    // latest为[fn1, fn2, fn4]
-    for (let i = 0; i < latest.length; i++) {
-      // push original options and not sealed options to exclude duplicated options
-      //ø ①extended有，就代表是.extend时传入的参数，是新增的，不是父类继承来的
-      //ø ②sealed没有，就代表这个是.extend之后mixin混入的，也是新增的，不是父类继承来的
-      // 就像这个Parent传入的fn4，是父类持有的，就不计入，之后统一contact
-      if (extended.indexOf(latest[i]) >= 0 || sealed.indexOf(latest[i]) < 0) {
-        res.push(latest[i])
-      }
+function dedupe(latest, extended, sealed) {
+    // compare latest and sealed to ensure lifecycle hooks won't be duplicated
+    // between merges
+    if (Array.isArray(latest)) {
+        // 因为是数组，那么一般都是生命周期钩子函数，去重就是保证不要重复触发
+        const res = []
+        // 承接上个栗子
+        // [fn1, fn2]
+        sealed = Array.isArray(sealed) ? sealed : [sealed]
+        // [fn2]
+        extended = Array.isArray(extended) ? extended : [extended]
+        // latest为[fn1, fn2, fn4]
+        for (let i = 0; i < latest.length; i++) {
+            // push original options and not sealed options to exclude duplicated options
+            //ø ①extended有，就代表是.extend时传入的参数，是新增的，不是父类继承来的
+            //ø ②sealed没有，就代表这个是.extend之后mixin混入的，也是新增的，不是父类继承来的
+            // 就像这个Parent传入的fn4，是父类持有的，就不计入，之后统一contact
+            if (extended.indexOf(latest[i]) >= 0 || sealed.indexOf(latest[i]) < 0) {
+                res.push(latest[i])
+            }
+        }
+        //ø 也就是这俩个不是父类继承来的（一个是.extend传参混入，一个是.mixin混入）
+        // [fn2, fn4]
+        return res
+    } else {
+        return latest
     }
-    //ø 也就是这俩个不是父类继承来的（一个是.extend传参混入，一个是.mixin混入）
-    // [fn2, fn4]
-    return res
-  } else {
-    return latest
-  }
 }
--- src/src/core/vdom/helpers/update-listeners.js
+++ src/src/core/vdom/helpers/update-listeners.js
@@ -2,82 +2,83 @@
 
 import { warn } from 'core/util/index'
 import { cached, isUndef, isPlainObject } from 'shared/util'
-
+// https://cn.vuejs.org/v2/guide/render-function.html#%E4%BA%8B%E4%BB%B6-amp-%E6%8C%89%E9%94%AE%E4%BF%AE%E9%A5%B0%E7%AC%A6
+// 根据名字的修饰符解析出该事件各个修饰符情况
 const normalizeEvent = cached((name: string): {
-  name: string,
-  once: boolean,
-  capture: boolean,
-  passive: boolean,
-  handler?: Function,
-  params?: Array<any>
+    name: string,
+    once: boolean,
+    capture: boolean,
+    passive: boolean,
+    handler?: Function,
+    params?: Array<any>
 } => {
-  const passive = name.charAt(0) === '&'
-  name = passive ? name.slice(1) : name
-  const once = name.charAt(0) === '~' // Prefixed last, checked first
-  name = once ? name.slice(1) : name
-  const capture = name.charAt(0) === '!'
-  name = capture ? name.slice(1) : name
-  return {
-    name,
-    once,
-    capture,
-    passive
-  }
+    const passive = name.charAt(0) === '&'
+    name = passive ? name.slice(1) : name
+    const once = name.charAt(0) === '~' // Prefixed last, checked first
+    name = once ? name.slice(1) : name
+    const capture = name.charAt(0) === '!'
+    name = capture ? name.slice(1) : name
+    return {
+        name,
+        once,
+        capture,
+        passive
+    }
 })
 
-export function createFnInvoker (fns: Function | Array<Function>): Function {
-  function invoker () {
-    const fns = invoker.fns
-    if (Array.isArray(fns)) {
-      const cloned = fns.slice()
-      for (let i = 0; i < cloned.length; i++) {
-        cloned[i].apply(null, arguments)
-      }
-    } else {
-      // return handler return value for single handlers
-      return fns.apply(null, arguments)
+export function createFnInvoker(fns: Function | Array<Function>): Function {
+    function invoker() {
+        const fns = invoker.fns
+        if (Array.isArray(fns)) {
+            const cloned = fns.slice()
+            for (let i = 0; i < cloned.length; i++) {
+                cloned[i].apply(null, arguments)
+            }
+        } else {
+            // return handler return value for single handlers
+            return fns.apply(null, arguments)
+        }
     }
-  }
-  invoker.fns = fns
-  return invoker
+    invoker.fns = fns
+    return invoker
 }
 
-export function updateListeners (
-  on: Object,
-  oldOn: Object,
-  add: Function,
-  remove: Function,
-  vm: Component
+export function updateListeners(
+    on: Object,
+    oldOn: Object,
+    add: Function,
+    remove: Function,
+    vm: Component
 ) {
-  let name, def, cur, old, event
-  for (name in on) {
-    def = cur = on[name]
-    old = oldOn[name]
-    event = normalizeEvent(name)
-    /* istanbul ignore if */
-    if (__WEEX__ && isPlainObject(def)) {
-      cur = def.handler
-      event.params = def.params
+    let name, def, cur, old, event
+    for (name in on) {
+        def = cur = on[name]
+        old = oldOn[name]
+        event = normalizeEvent(name)
+        /* istanbul ignore if */
+        if (__WEEX__ && isPlainObject(def)) {
+            cur = def.handler
+            event.params = def.params
+        }
+        if (isUndef(cur)) {
+            process.env.NODE_ENV !== 'production' && warn(
+                `Invalid handler for event "${event.name}": got ` + String(cur),
+                vm
+            )
+        } else if (isUndef(old)) {
+            if (isUndef(cur.fns)) {
+                cur = on[name] = createFnInvoker(cur)
+            }
+            add(event.name, cur, event.once, event.capture, event.passive, event.params)
+        } else if (cur !== old) {
+            old.fns = cur
+            on[name] = old
+        }
     }
-    if (isUndef(cur)) {
-      process.env.NODE_ENV !== 'production' && warn(
-        `Invalid handler for event "${event.name}": got ` + String(cur),
-        vm
-      )
-    } else if (isUndef(old)) {
-      if (isUndef(cur.fns)) {
-        cur = on[name] = createFnInvoker(cur)
-      }
-      add(event.name, cur, event.once, event.capture, event.passive, event.params)
-    } else if (cur !== old) {
-      old.fns = cur
-      on[name] = old
+    for (name in oldOn) {
+        if (isUndef(on[name])) {
+            event = normalizeEvent(name)
+            remove(event.name, oldOn[name], event.capture)
+        }
     }
-  }
-  for (name in oldOn) {
-    if (isUndef(on[name])) {
-      event = normalizeEvent(name)
-      remove(event.name, oldOn[name], event.capture)
-    }
-  }
 }
